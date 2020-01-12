45599e532ffc6e2e34a9663db63b9a467a1800fc
VeronicaDem <veron.dem4enko2014@yandex.ru>
2020-01-12 22:43:26 +0300

    Updates

---
+++ precache-manifest.7474655bf9087bd4d77caca376e91841.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690433650835497593fb3387b26898ee",
+    "url": "/group-app/index.html"
+  },
+  {
+    "revision": "39af09640271904e70b9",
+    "url": "/group-app/static/js/2.542960b1.chunk.js"
+  },
+  {
+    "revision": "19ff70a36140f0a830cc",
+    "url": "/group-app/static/js/main.53aa0da3.chunk.js"
+  },
+  {
+    "revision": "b258e098614e2fc6f897",
+    "url": "/group-app/static/js/runtime~main.6a441d88.js"
+  }
+]);
